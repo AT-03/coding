02f2ee2605e5b77b72c20c9339d208fd220ce7fe
reianldo <reyal@hotmail.com>
2017-03-11 01:56:26 +0000

    First task in gitHub

---
+++ src/main/java/org/fundacionjala/coding/BankOCR/BasnkOCRHistory.java
@@ -0,0 +1,15 @@
+package org.fundacionjala.coding.BankOCR;
+
+/**
+ * Created by Administrator on 3/10/2017.
+ */
+public class BasnkOCRHistory {
+    public int fillNumber(){
+        Map<Integer, String> nombreMap = new HashMap<Integer, String>();
+        return 0;
+    }
+    public String bankVerificate(String fourDate)
+    {
+        return null;
+    }
+}
