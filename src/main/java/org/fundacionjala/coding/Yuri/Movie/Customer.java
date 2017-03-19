3a1ef62229c537e0ce3c4f1cb2b8a10bd35eb8be
yvayne <yurivayne@gmail.com>
2017-03-19 17:25:36 -0400

    fifth commit

--- src/main/java/org/fundacionjala/coding/Yuri/Movie/Customer.java
+++ src/main/java/org/fundacionjala/coding/Yuri/Movie/Customer.java
@@ -4,25 +4,25 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 class Customer {
-    private String _name;
-    private Vector _rentals = new Vector();
+    private String name;
+    private Vector rentals = new Vector();
 
     public Customer(String name) {
-        _name = name;
+        this.name = name;
     }
 
     public void addRental(Rental arg) {
-        _rentals.addElement(arg);
+        rentals.addElement(arg);
     }
 
     public String getName() {
-        return _name;
+        return name;
     }
 
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration rentals = _rentals.elements();
+        Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
@@ -37,7 +37,7 @@ class Customer {
                 case Movie.NEW_RELEASE:
                     thisAmount += each.getDaysRented() * 3;
                     break;
-                case Movie.CHILDRENS:
+                case Movie.CHILDREN:
                     thisAmount += 1.5;
                     if (each.getDaysRented() > 3)
                         thisAmount += (each.getDaysRented() - 3) * 1.5;
