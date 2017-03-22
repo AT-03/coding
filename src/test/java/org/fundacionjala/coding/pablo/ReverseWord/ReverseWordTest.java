6218959282d76e39ab91e07c938d0ff57b2ea9f1
pablopzsj <pablo.ramirez@fundacion-jala.org>
2017-03-21 23:12:55 -0400

    RW test

--- src/test/java/org/fundacionjala/coding/pablo/ReverseWord/ReverseWordTest.java
+++ src/test/java/org/fundacionjala/coding/pablo/ReverseWord/ReverseWordTest.java
@@ -1,8 +1,27 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.pablo.ReverseWord;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Administrator on 3/21/2017.
  */
 public class ReverseWordTest {
+
+    /**
+     * Test about ReverseWord class.
+     */
+
+    @Test
+    public void verify() {
+
+        final String input = "Hi how are you my friend";
+
+        final String actual = ReverseWord.reverseWord(input);
+
+        final String expected = "Hi how are you my dneirf";
+        assertEquals(expected, actual);
+    }
 
 }
