1968882f381d90c8bd081646c24c4199a8f915cf
reianldo <reyal@hotmail.com>
2017-03-23 20:27:38 +0000

    bank

--- src/main/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcr.java
+++ src/main/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcr.java
@@ -1,7 +1,199 @@
 package org.fundacionjala.coding.reinaldoA.spinWords.bankOcr;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Administrator on 3/23/2017.
  */
-public class BankOcr {
+public final class BankOcr {
+
+    private static final int MODULUS_FACTOR = 11;
+    private static final int CORRECT_ACCOUNT_LENGTH = 9;
+    private static final int CORRECT_SCANNED_ACCOUNT_LENGTH = 72;
+    private static final int MULTIPLY_FACTOR = 9;
+    private static final int INDEX_MODULUS_FACTOR = 9;
+    private static final int SUB_STRING_START = 0;
+    private static final int SUB_STRING_END = 3;
+    private static final Map<Integer, String> STRING_MAP = new HashMap<>();
+
+    static {
+        STRING_MAP.put(KEY.ZERO.ordinal(),
+                " _ "
+                        + "| |"
+                        + "|_|");
+        STRING_MAP.put(KEY.ONE.ordinal(),
+                "   "
+                        + "  |"
+                        + "  |");
+        STRING_MAP.put(KEY.TWO.ordinal(),
+                " _ "
+                        + " _|"
+                        + "|_ ");
+        STRING_MAP.put(KEY.THREE.ordinal(),
+                "__ "
+                        + " _|"
+                        + "__|");
+        STRING_MAP.put(KEY.FOUR.ordinal(),
+                "   "
+                        + "|_|"
+                        + "  |");
+        STRING_MAP.put(KEY.FIVE.ordinal(),
+                " _ "
+                        + "|_ "
+                        + " _|");
+        STRING_MAP.put(KEY.SIX.ordinal(),
+                " _ "
+                        + "|_ "
+                        + "|_|");
+        STRING_MAP.put(KEY.SEVEN.ordinal(),
+                "__ "
+                        + "  |"
+                        + "  |");
+        STRING_MAP.put(KEY.EIGHT.ordinal(),
+                " _ "
+                        + "|_|"
+                        + "|_|");
+        STRING_MAP.put(KEY.NINE.ordinal(),
+                " _ "
+                        + "|_|"
+                        + " _|");
+    }
+
+    /**
+     * Constructor private.
+     */
+    private BankOcr() {
+    }
+
+    /**
+     * @param value Maping the date in my contain
+     * @return "?" if the value isnot digit,
+     * return a number between 0 and 9.
+     */
+    private static String comparationKey(final String value) {
+        String key = "?";
+        for (Map.Entry<Integer, String> entry :STRING_MAP.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                key = entry.getKey().toString();
+            }
+        }
+        return key;
+    }
+
+    /**
+     * This method take and scanned account of exactly 9 digits and return
+     * an array representation of them.
+     *
+     * @param scannedAccount String, represents the account.
+     * @return an array of nine string digits.
+     */
+    public static String[] parseScannedNumbers(final String scannedAccount) {
+
+        String[] scannedDigits = {"", "", "", "", "", "", "", "", ""};
+
+        if (!lengthScannedImageIsValid(scannedAccount)) {
+            int index = 0;
+            int start = SUB_STRING_START;
+            int end = SUB_STRING_END;
+
+            for (int i = 0; i < scannedAccount.length(); i += SUB_STRING_END) {
+                index = index % INDEX_MODULUS_FACTOR;
+
+                scannedDigits[index] += scannedAccount.substring(start, end);
+
+                start = end;
+                end += SUB_STRING_END;
+                index++;
+            }
+        }
+        return scannedDigits;
+    }
+
+    /**
+     * This method determines if an scanned account is legible.
+     *
+     * @param account image of type String.
+     * @return true or false
+     */
+    private static boolean isNumber(final String account) {
+        boolean isCorrect = true;
+
+        for (int i = 0; i < account.length(); i++) {
+            if (!Character.isDigit(account.charAt(i))) {
+                isCorrect = false;
+                break;
+            }
+        }
+        return  isCorrect;
+    }
+
+    /**
+     * This method validate if an account is correct or not.
+     *
+     * @param account String variable is the account to validate
+     * @return true if the passed account is true, false otherwise.
+     */
+    public static boolean validateAccount(final String account) {
+        if (account.length() == CORRECT_ACCOUNT_LENGTH && isNumber(account)) {
+
+            String[] acct = account.split("");
+
+            int checksum = 0;
+            int factor = MULTIPLY_FACTOR;
+
+            for (String value : acct) {
+                checksum += Integer.parseInt(value) * factor;
+                factor--;
+            }
+
+            return checksum % MODULUS_FACTOR == 0;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * This method is in charge to give one of three statues to the account".
+     * "", "ERR" or "ILL"
+     *
+     * @param account String variable to determine its status.
+     * @return an String result with the values mentioned earlier.
+     */
+    public static String getAccountStatus(final String account) {
+        String status = "";
+
+        if (!isNumber(account)) {
+            status = "ILL";
+        } else if (!validateAccount(account)) {
+            status = "ERR";
+        }
+        return status;
+    }
+
+
+    /**
+     * @param linesScannedImage this is mi data insert lines
+     * @return String return string in format digit
+     */
+    public static String accountRepresentation(final String[] linesScannedImage) {
+        StringBuilder resultaRepresentation = new StringBuilder();
+
+        for (String number :  linesScannedImage) {
+            resultaRepresentation.append(comparationKey(number));
+        }
+
+        return  resultaRepresentation.toString();
+    }
+
+    /**
+     * This method validates if an scanned account has the correct digit
+     * numbers.
+     *
+     * @param scannedAccount of String type.
+     * @return true.
+     */
+    private static boolean lengthScannedImageIsValid(final String scannedAccount) {
+        return scannedAccount.length() == CORRECT_SCANNED_ACCOUNT_LENGTH;
+    }
 }
--- src/test/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcrTest.java
+++ src/test/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcrTest.java
@@ -1,8 +1,292 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.reinaldoA.spinWords.bankOcr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Administrator on 3/23/2017.
  */
 public class BankOcrTest {
 
+    /**
+     * Test if test Bank Ocr When Scanned Numbers Cero.
+     */
+    @Test
+    public void testBankOcrWhenScannedNumbersCero() {
+        // given:
+        String[] sentenceImage = {
+                " _ "
+                        + "| |"
+                        + "|_|"
+        };
+        // when:
+        String actualResult = BankOcr.accountRepresentation(sentenceImage);
+
+        // then:
+        String expectedResult = "0";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Test if test Bank Ocr When Scanned Numbers Cero To Nine.
+     */
+    @Test
+    public void testBankOcrWhenScannedNumbersCeroToNine() {
+        // given:
+        String[] sentenceImage = {
+                " _ "
+                        + "| |"
+                        + "|_|",
+
+                "   "
+                        + "  |"
+                        + "  |",
+
+                " _ "
+                        + " _|"
+                        + "|_ ",
+
+                "__ "
+                        + " _|"
+                        + "__|",
+
+                "   "
+                        + "|_|"
+                        + "  |",
+
+                " _ "
+                        + "|_ "
+                        + " _|",
+
+                " _ "
+                        + "|_ "
+                        + "|_|",
+
+                "__ "
+                        + "  |"
+                        + "  |",
+
+                " _ "
+                        + "|_|"
+                        + "|_|",
+
+                " _ "
+                        + "|_|"
+                        + " _|"
+        };
+
+        // when:
+        String actualResult = BankOcr.accountRepresentation(sentenceImage);
+
+        // then:
+        String expectedResult = "0123456789";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Test that anything different from a number is recognized as '?'.
+     */
+    @Test
+    public void testBankOcrKeyWhenScannedImageHasDifferentValues() {
+        // given:
+        String[] sentenceImage = {
+                " _ "
+                        + "| |"
+                        + "|_|",
+
+                " _ "
+                        + "|_|"
+                        + " _|",
+
+                "This is different",
+                "",
+                " _ "
+                        + "|_ "
+                        + " _|"
+        };
+
+        // when:
+        String actualResult = BankOcr.accountRepresentation(sentenceImage);
+
+        // then:
+        String expectedResult = "09??5";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Test if the scanned account is a correct one.
+     */
+    @Test
+    public void testBankOcrValidateAccountWhenTheGivenAccountIsCorrect() {
+        // given:
+        String[] sentenceImage = {
+                "__ "
+                        + " _|"
+                        + "__|",
+
+                "   "
+                        + "|_|"
+                        + "  |",
+
+                " _ "
+                        + "|_ "
+                        + " _|",
+
+                " _ "
+                        + "|_|"
+                        + "|_|",
+
+                " _ "
+                        + "|_|"
+                        + "|_|",
+
+                " _ "
+                        + " _|"
+                        + "|_ ",
+
+                " _ "
+                        + "|_|"
+                        + "|_|",
+
+                " _ "
+                        + "|_ "
+                        + "|_|",
+
+                " _ "
+                        + "|_ "
+                        + " _|",
+        };
+        String correctAccount = BankOcr.accountRepresentation(sentenceImage);
+
+        // when:
+        boolean actualResult = BankOcr.validateAccount(correctAccount);
+
+        // then:
+        assertTrue(actualResult);
+
+    }
+
+    /**
+     * Test if the scanned account is an incorrect one.
+     */
+    @Test
+    public void testBankOcrValidateAccountWhenTheGivenAccountIsIncorrect() {
+        // given:
+        String[] sentenceImage = {
+                "   "
+                        + "|_|"
+                        + "  |",
+
+                " _ "
+                        + "|_ "
+                        + " _|",
+
+                " _ "
+                        + "|_ "
+                        + "|_|",
+
+                "__ "
+                        + "  |"
+                        + "  |",
+
+        };
+        String incorrectAccount = BankOcr.accountRepresentation(sentenceImage);
+
+        // when:
+        boolean actualResult = BankOcr.validateAccount(incorrectAccount);
+
+        // then:
+        assertTrue(!actualResult);
+    }
+
+    /**
+     * Test if the status of a correct account is "".
+     */
+    @Test
+    public void testBankOcrAccountStatusWithACorrectAccount() {
+        // given:
+        String sentenceAccount = "345882865";
+
+        // when:
+        String actualResult = BankOcr.getAccountStatus(sentenceAccount);
+
+        // then:
+        String expectResult = "";
+        assertEquals(expectResult, actualResult);
+    }
+
+    /**
+     * Test if the Account Status With An IncorrectAccount.
+     */
+    @Test
+    public void testBankOcrAccountStatusWithAnIncorrectAccount() {
+        // given:
+        String sentenceAccount = "021453789";
+
+        // when:
+        String actualResult = BankOcr.getAccountStatus(sentenceAccount);
+
+        // then:
+        String expectedResult = "ERR";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Test if the Account Status With An Illegible Account.
+     */
+    @Test
+    public void testBankOcrAccountStatusWithAnIllegibleAccount() {
+        // given:
+        String sentenceAccount = "  664371?95";
+
+        // when:
+        String actualResult = BankOcr.getAccountStatus(sentenceAccount);
+
+        // then:
+        String expectedResult = "ILL";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Test if a Scanned Number With Incorrect Number Of Digits.
+     */
+    @Test
+    public void testBankOcrParseScannedNumberWithIncorrectNumberOfDigits() {
+        //given:
+        String scannedString =
+                " _ "
+                        + "| |"
+                        + "|_|";
+        String[] accountArray = BankOcr.parseScannedNumbers(scannedString);
+
+        // when:
+        String actualResult = BankOcr.accountRepresentation(accountArray);
+
+        // then:
+        String expectedResult = "?????????";
+        assertEquals(expectedResult, actualResult);
+    }
+
+    /**
+     * Test if a Scanned Numbers With Nine Digits.
+     */
+    @Test
+    public void testBankOcrParseScannedNumbersWithNineDigits() {
+        // given:
+        String scannedString =
+                "__     _  _  _  _  _  _  _ "
+                        + " _||_||_ |_||_| _||_||_ |_ "
+                        + "__|  | _||_||_||_ |_||_| _|";
+
+        String[] accountArray = BankOcr.parseScannedNumbers(scannedString);
+
+        // when:
+        String actualResult = BankOcr.accountRepresentation(accountArray);
+
+        // then:
+        String expectedResult = "345882865";
+        assertEquals(expectedResult, actualResult);
+    }
 }
