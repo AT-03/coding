63e3ac9178207f56042998ea18c444fe9692ca6d
reianldo <reyal@hotmail.com>
2017-03-23 20:24:00 +0000

    bank

---
+++ src/main/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/KEY.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.reinaldoA.spinWords.bankOcr;
+
+/**
+ * Created by Administrator on 3/23/2017.
+ */
+public enum KEY {
+}
---
+++ src/test/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcrTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 3/23/2017.
+ */
+public class BankOcrTest {
+
+}
