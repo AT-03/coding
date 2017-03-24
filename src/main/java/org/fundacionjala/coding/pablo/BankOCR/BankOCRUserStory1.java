d3ac7aaa129c5db2c0d01fc80a13e57e570b4574
pablopzsj <pablo.ramirez@fundacion-jala.org>
2017-03-23 20:15:22 -0400

    update

---
+++ src/main/java/org/fundacionjala/coding/pablo/BankOCR/BankOCRUserStory1.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.pablo.BankOCR;
+
+/**
+ * Created by Administrator on 3/23/2017.
+ */
+public class BankOCRUserStory1 {
+}
---
+++ src/main/java/org/fundacionjala/coding/pablo/Movies/Rental.java
@@ -0,0 +1,7 @@
+package org.fundacionjala.coding.pablo.Movies;
+
+/**
+ * Created by Administrator on 3/23/2017.
+ */
+public class Rental {
+}
