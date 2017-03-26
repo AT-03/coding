93f81bfeccb513a0782ef188eb15cb1979591fc4
jpaitken <juan.aitken@fundacion-jala.org>
2017-03-26 01:27:43 -0400

    Commit Test

---
+++ src/main/java/org/fundacionjala/coding/juan/test/Arrayaarage.java
@@ -0,0 +1,32 @@
+package org.fundacionjala.coding.juan.test;
+
+/**
+ * Created by Administrator on 3/23/2017.
+ */
+final class Arrayaarage {
+
+    /**
+     * constructor.
+     */
+    private Arrayaarage() {
+    }
+
+    /**
+     * this function will calculate the averrage of a given array.
+     *
+     * @param arrayInt is the given array
+     * @return final array.
+     */
+    public static double[] getArrayAverage(final int[] arrayInt) {
+        if (arrayInt == null || arrayInt.length == 0 || arrayInt.length == 1) {
+            return new double[0];
+        }
+        double[] finalArray = new double[arrayInt.length - 1];
+        for (int i = 0; i < arrayInt.length - 1; i++) {
+            finalArray[i] = (double) (arrayInt[i] + arrayInt[i + 1]) / 2;
+
+        }
+        return finalArray;
+    }
+
+}
