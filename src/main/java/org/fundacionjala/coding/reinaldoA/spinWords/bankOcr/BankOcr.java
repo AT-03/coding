3674fee00d0d09284e13fd9d9a37413b1f87b399
ReinaldoAguilar <ReinaldoAguilar@users.noreply.github.com>
2017-05-22 18:48:07 -0400

    Update BankOcr.java

--- src/main/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcr.java
+++ src/main/java/org/fundacionjala/coding/reinaldoA/spinWords/bankOcr/BankOcr.java
@@ -4,196 +4,111 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Administrator on 3/23/2017.
+ * Created by reinaldo on 26/03/2017.
  */
 public final class BankOcr {
 
-    private static final int MODULUS_FACTOR = 11;
-    private static final int CORRECT_ACCOUNT_LENGTH = 9;
-    private static final int CORRECT_SCANNED_ACCOUNT_LENGTH = 72;
-    private static final int MULTIPLY_FACTOR = 9;
-    private static final int INDEX_MODULUS_FACTOR = 9;
-    private static final int SUB_STRING_START = 0;
-    private static final int SUB_STRING_END = 3;
-    private static final Map<Integer, String> STRING_MAP = new HashMap<>();
+    private static Map<Integer, String> mapNum = new HashMap<>();
+
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int THREE = 3;
+    private static final int FOUR = 4;
+    private static final int FIVE = 5;
+    private static final int SIX = 6;
+    private static final int SEVEN = 7;
+    private static final int EIGHT = 8;
+    private static final int NINE = 9;
 
     static {
-        STRING_MAP.put(KEY.ZERO.ordinal(),
+        mapNum.put(ZERO,
                 " _ "
                         + "| |"
                         + "|_|");
-        STRING_MAP.put(KEY.ONE.ordinal(),
+        mapNum.put(ONE,
                 "   "
                         + "  |"
                         + "  |");
-        STRING_MAP.put(KEY.TWO.ordinal(),
+
+        mapNum.put(TWO,
                 " _ "
                         + " _|"
                         + "|_ ");
-        STRING_MAP.put(KEY.THREE.ordinal(),
+
+        mapNum.put(THREE,
                 "__ "
                         + " _|"
                         + "__|");
-        STRING_MAP.put(KEY.FOUR.ordinal(),
+
+        mapNum.put(FOUR,
                 "   "
                         + "|_|"
                         + "  |");
-        STRING_MAP.put(KEY.FIVE.ordinal(),
-                " _ "
+
+        mapNum.put(FIVE,
+                " _"
                         + "|_ "
                         + " _|");
-        STRING_MAP.put(KEY.SIX.ordinal(),
+
+        mapNum.put(SIX,
                 " _ "
                         + "|_ "
                         + "|_|");
-        STRING_MAP.put(KEY.SEVEN.ordinal(),
-                "__ "
+
+        mapNum.put(SEVEN,
+                " _ "
                         + "  |"
                         + "  |");
-        STRING_MAP.put(KEY.EIGHT.ordinal(),
+        mapNum.put(EIGHT,
                 " _ "
                         + "|_|"
                         + "|_|");
-        STRING_MAP.put(KEY.NINE.ordinal(),
+
+        mapNum.put(NINE,
                 " _ "
                         + "|_|"
                         + " _|");
     }
 
     /**
-     * Constructor private.
+     * Constructor.
      */
+
     private BankOcr() {
+
     }
 
     /**
-     * @param value Maping the date in my contain
-     * @return "?" if the value isnot digit,
-     * return a number between 0 and 9.
-     */
-    private static String comparationKey(final String value) {
-        String key = "?";
-        for (Map.Entry<Integer, String> entry :STRING_MAP.entrySet()) {
-            if (entry.getValue().equals(value)) {
-                key = entry.getKey().toString();
-            }
-        }
-        return key;
-    }
-
-    /**
-     * This method take and scanned account of exactly 9 digits and return
-     * an array representation of them.
      *
-     * @param scannedAccount String, represents the account.
-     * @return an array of nine string digits.
+     * @param value to get int.
+     * @return characters in a range 0-9
      */
-    public static String[] parseScannedNumbers(final String scannedAccount) {
 
-        String[] scannedDigits = {"", "", "", "", "", "", "", "", ""};
-
-        if (!lengthScannedImageIsValid(scannedAccount)) {
-            int index = 0;
-            int start = SUB_STRING_START;
-            int end = SUB_STRING_END;
-
-            for (int i = 0; i < scannedAccount.length(); i += SUB_STRING_END) {
-                index = index % INDEX_MODULUS_FACTOR;
-
-                scannedDigits[index] += scannedAccount.substring(start, end);
-
-                start = end;
-                end += SUB_STRING_END;
-                index++;
-            }
+    private static String getKey(final String value) {
+        String valueString = "?";
+        for (Map.Entry<Integer, String> inputData : mapNum.entrySet()) {
+            valueString = inputData.getValue().equalsIgnoreCase(value)
+                    ? inputData.getKey().toString() : valueString;
         }
-        return scannedDigits;
+        return valueString;
     }
 
     /**
-     * This method determines if an scanned account is legible.
      *
-     * @param account image of type String.
+     * @param numberScanned String
      * @return true or false
      */
-    private static boolean isNumber(final String account) {
-        boolean isCorrect = true;
 
-        for (int i = 0; i < account.length(); i++) {
-            if (!Character.isDigit(account.charAt(i))) {
-                isCorrect = false;
-                break;
-            }
-        }
-        return  isCorrect;
-    }
+    public static String convertStringToNumber(final String[] numberScanned) {
+        StringBuilder imageToString = new StringBuilder();
 
-    /**
-     * This method validate if an account is correct or not.
-     *
-     * @param account String variable is the account to validate
-     * @return true if the passed account is true, false otherwise.
-     */
-    public static boolean validateAccount(final String account) {
-        if (account.length() == CORRECT_ACCOUNT_LENGTH && isNumber(account)) {
-
-            String[] acct = account.split("");
-
-            int checksum = 0;
-            int factor = MULTIPLY_FACTOR;
-
-            for (String value : acct) {
-                checksum += Integer.parseInt(value) * factor;
-                factor--;
-            }
-
-            return checksum % MODULUS_FACTOR == 0;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * This method is in charge to give one of three statues to the account".
-     * "", "ERR" or "ILL"
-     *
-     * @param account String variable to determine its status.
-     * @return an String result with the values mentioned earlier.
-     */
-    public static String getAccountStatus(final String account) {
-        String status = "";
-
-        if (!isNumber(account)) {
-            status = "ILL";
-        } else if (!validateAccount(account)) {
-            status = "ERR";
-        }
-        return status;
-    }
-
-
-    /**
-     * @param linesScannedImage this is mi data insert lines
-     * @return String return string in format digit
-     */
-    public static String accountRepresentation(final String[] linesScannedImage) {
-        StringBuilder resultaRepresentation = new StringBuilder();
-
-        for (String number :  linesScannedImage) {
-            resultaRepresentation.append(comparationKey(number));
+        for (String numberString : numberScanned) {
+            imageToString.append(getKey(numberString));
         }
 
-        return  resultaRepresentation.toString();
-    }
-
-    /**
-     * This method validates if an scanned account has the correct digit
-     * numbers.
-     *
-     * @param scannedAccount of String type.
-     * @return true.
-     */
-    private static boolean lengthScannedImageIsValid(final String scannedAccount) {
-        return scannedAccount.length() == CORRECT_SCANNED_ACCOUNT_LENGTH;
+        return imageToString.toString();
     }
 }
+
+
