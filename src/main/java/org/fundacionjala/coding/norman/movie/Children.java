7e3d84df763df3c819aaafad9517428a8360bdbe
666nonak666 <norman.antezana@fundacion-jala.org>
2017-05-22 17:59:10 +0000

    Update Children.java

--- src/main/java/org/fundacionjala/coding/norman/movie/Children.java
+++ src/main/java/org/fundacionjala/coding/norman/movie/Children.java
@@ -1,13 +1,14 @@
-package org.fundacionjala.coding.norman.movie;
 
 /**
- * Created by Administrator on 3/21/2017.
+ * Created by Norman on 3/21/2017.
  */
 public class Children extends Movie {
     private static final int LIMITNEWCHILD = 3;
     private static final double PRICE = 1.5;
+
     /**
      * contructor children.
+     *
      * @param title the name
      */
     public Children(final String title) {
@@ -16,17 +17,18 @@ public class Children extends Movie {
 
     @Override
     public double calculateAmount(final int daysRented) {
-      //  double amount = PRICE;
-       // if (daysRented > LIMITNEWCHILD) {
+        //  double amount = PRICE;
+        // if (daysRented > LIMITNEWCHILD) {
         //    amount += (daysRented - LIMITNEWCHILD) * PRICE;
-      //  }
-       // return amount;
+        //  }
+        // return amount;
         //sum += i % 2 == 0 ? numericValue * DIGIT_MULTIPLIER : numericValue;
-        return daysRented > LIMITNEWCHILD ? ((daysRented - LIMITNEWCHILD) * PRICE)+PRICE : PRICE ;
+        return daysRented > LIMITNEWCHILD ? ((daysRented - LIMITNEWCHILD) * PRICE) + PRICE : PRICE;
     }
+
     /**
      * @param daysRented .
-     * return double of calculateFrequentRenterPoints.
+     *                   return double of calculateFrequentRenterPoints.
      */
     @Override
     public int calculateFrequentRenterPoints(final int daysRented) {
