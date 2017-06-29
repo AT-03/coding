56336f4f419b2d99b100ef29ad5b976c1875e622
Ruber Cuellar <ruber.cuellar@jalasoft.com>
2017-06-29 10:25:51 -0400

    Adding Evaporator

---
+++ src/main/java/org/fundacionjala/coding/Ruber/Evaporator/Evaporator.java
@@ -0,0 +1,34 @@
+package org.fundacionjala.coding.Ruber.Evaporator;
+
+/**
+ * Created by Ruber Cuellar on 6/28/2017.
+ */
+public final class Evaporator {
+
+    private static final int VALUE_PERCENTAGE = 100;
+
+    /**
+     * Constructor.
+     */
+    private Evaporator() {
+
+    }
+
+    /**
+     * To get days for evaporing.
+     * @param content The content
+     * @param percentage The percentage to lose per day
+     * @param limit The limit to not lose
+     * @return int
+     */
+    public static int evaporator(final double content, final int percentage, final int limit) {
+
+        int days = 0;
+        double actualContent = content;
+        while (actualContent >= (content * limit / VALUE_PERCENTAGE)) {
+            actualContent -= (actualContent * percentage) / VALUE_PERCENTAGE;
+            days++;
+        }
+        return days;
+    }
+}
