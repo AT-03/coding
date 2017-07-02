9a42cae7c9dd8366f72c2ad94d9634fc4af3d044
pablopzsj <pablo.ramirez@fundacion-jala.org>
2017-07-01 22:35:45 -0400

    customer test

--- src/main/java/org/fundacionjala/coding/pablo/Movies/Customer.java
+++ src/main/java/org/fundacionjala/coding/pablo/Movies/Customer.java
@@ -41,7 +41,8 @@ public class Customer {
         result.append("Rental Record for ").append(getName()).append("\n");
         for (Rental rental : rentalsCustomer) {
             result.append("\t").append(rental.getMovie().getTitle()).append("\t");
-            result.append(rental.calculateAmount()).append("\n");
+            result.append("\t").append(rental.calculateAmount()).append("\t");
+            result.append("Days Rental ").append(rental.getDaysRented()).append("\n");
         }
         result.append("Amount owed is ").append(calculateTotalAmount()).append("\n");
         result.append("You earned ").append(calculateTotalFrequentRenterPoints()).append(" frequent renter points");
@@ -67,7 +68,7 @@ public class Customer {
     public int calculateTotalFrequentRenterPoints() {
 
         return rentalsCustomer.stream()
-                .mapToInt(rental -> rental.calculateFrequentRenterPoint())
+                .mapToInt(Rental::calculateFrequentRenterPoint)
                 .sum();
     }
 }
--- src/test/java/org/fundacionjala/coding/pablo/Movies/CustomerTest.java
+++ src/test/java/org/fundacionjala/coding/pablo/Movies/CustomerTest.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Administrator on 7/1/2017.
+ * Created by PABLO on 7/1/2017.
  */
 public class CustomerTest {
+
 
     /**
      * Test when the customer name is given.
@@ -46,5 +47,27 @@ public class CustomerTest {
         final double expectedResult = 1.5;
 
         assertEquals(expectedResult, actualResult, 0);
+    }
+
+    /**
+     * Test customer class.
+     */
+    @Test
+    public void testCustomer() {
+        // given
+        Customer customer = new Customer("Pablo");
+        customer.addRental(new Rental(new NewRelease("The Avengers"), 2));
+        customer.addRental(new Rental(new Regular("Iron Man"), 1));
+
+        // when
+        final String actualResult = customer.generateDetail();
+
+        // then
+        final String expectedResult = "Rental Record for Pablo\n"
+                + "\tThe Avengers\t\t6.0\tDays Rental 2\n"
+                + "\tIron Man\t\t2.0\tDays Rental 1\n"
+                + "Amount owed is 8.0\n"
+                + "You earned 3 frequent renter points";
+        assertEquals(actualResult, expectedResult);
     }
 }
